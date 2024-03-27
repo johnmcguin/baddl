f140bf8e316d0f0e746698ddd503f8034279d516
John McGuin <john@waysoftware.dev>
2024-03-27 14:18:28 -0600

    fix: use app live view with layout preconfigured

--- lib/baddl_web/live/wordle_live.ex
+++ lib/baddl_web/live/wordle_live.ex
@@ -1,9 +1,13 @@
 defmodule BaddlWeb.WordleLive do
-  use Phoenix.LiveView
+  use BaddlWeb, :live_view
 
   def render(assigns) do
     ~H"""
     <div id="wordle-game" phx-hook="Wordle" phx-update="ignore"></div>
     """
+  end
+
+  def mount(params, session, socket) do
+    {:ok, socket}
   end
 end
