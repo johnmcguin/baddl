86c80d81764f9560cc0668ef35c9e70f40c969a7
John McGuin <john@waysoftware.dev>
2024-04-02 15:24:21 -0600

    task: remove some helper comments

--- lib/baddl_web/live/wordle_live.ex
+++ lib/baddl_web/live/wordle_live.ex
@@ -12,9 +12,6 @@ defmodule BaddlWeb.WordleLive do
   alias Baddl.Games
 
   def render(assigns) do
-    # will likely need to loop over an array of n number 
-    # game players. For each player that is not the current user,
-    # show their status of guesses
     ~H"""
     <div class="messages"><%= @name %></div>
     <div :if={@msg}><%= @msg %></div>
@@ -22,13 +19,7 @@ defmodule BaddlWeb.WordleLive do
     """
   end
 
-  # get the cookie from a plug and assign it to the session.
-  # configure the cookie to be long lived. Use this as client 
-  # id 
   def mount(%{"id" => id}, _session, socket) do
-    # Subscribe to the game topic
-    # e.g. Endpoint.subscribe("game:<game_id>")
-    # assign any relevant info to the socket (may not be any right now)
     case Games.get_room_by(short_token: id) do
       nil ->
         socket =
@@ -69,25 +60,8 @@ defmodule BaddlWeb.WordleLive do
 
   # handles info broadcast from other LiveViews
   def handle_info(%{topic: _topic, event: "handle_player_guess", payload: payload}, socket) do
-    {:noreply, assign(socket, msg: "hello from another liveview")}
+    {:noreply, assign(socket, msg: "hello from #{payload.player}")}
   end
-
-  # handle_event receives events from the game client
-  def handle_event(event, value, socket) do
-    # broadcast the event from pid's self so that the other liveviews
-    # get notified
-    # Endpoint.broadcast_from(self(), @topic, step, state)
-    # may only need to broadcast since client who gave their update does not need to be updated (elm is showing them this state)
-    # If needed, can send message to self()
-    # send(self(), next_step)
-
-    {:noreply, socket}
-  end
-
-  # handle_info receives messages from other liveviews
-  # def handle_info(_msg, socket) do
-  #   {:noreply, socket}
-  # end
 
   def assign_current_player(socket, name) do
     assign(socket, name: name)
