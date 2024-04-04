a3e0133b4e06de7bdbbc0f316a69d0248a59956f
John McGuin <john@waysoftware.dev>
2024-04-04 15:15:41 -0600

    feat(wip): update other live views for given player

--- lib/baddl_web/live/wordle_live.ex
+++ lib/baddl_web/live/wordle_live.ex
@@ -13,10 +13,23 @@ defmodule BaddlWeb.WordleLive do
 
   def render(assigns) do
     ~H"""
-    <div><%= @name %></div>
-    <div><%= @messages %></div>
     <.async_result :let={answer} assign={@answer}>
       <:failed :let={_failure}>there was an error creating the game</:failed>
+      <div :if={@messages} class="game-meta">
+        <div :for={{player, summary} <- @messages} class="flex flex-row mb-4">
+          <span class="mr-4"><%= player %>:</span>
+          <div>
+            <%= Enum.with_index(summary.latest_guess, fn guess, idx -> %>
+              <span
+                class={"reveal #{String.downcase(guess)} w-3 h-3 inline-block"}
+                style={"animation-delay: #{idx * 0.1}s"}
+              >
+              </span>
+            <% end) %>
+            <span class="mr-4">(attempt <%= summary.latest_guess_num %>)</span>
+          </div>
+        </div>
+      </div>
       <div id="wordle-game" phx-hook="Wordle" phx-update="ignore" data-answer={answer}></div>
     </.async_result>
     """
@@ -36,7 +49,7 @@ defmodule BaddlWeb.WordleLive do
         socket
         |> assign(name: name)
         |> assign(room_id: id)
-        |> assign(messages: nil)
+        |> assign(messages: %{})
         |> assign_async(:answer, fn -> get_answer(id) end)
         |> then(fn socket -> {:noreply, socket} end)
     end
@@ -46,10 +59,11 @@ defmodule BaddlWeb.WordleLive do
     {:noreply, push_patch(socket, to: "/game/#{id}?name=Guest")}
   end
 
-  def handle_event("handle_guess", made_guess, socket) do
+  def handle_event("handle_guess", payload, socket) do
     Endpoint.broadcast_from(self(), "game:#{socket.assigns.room_id}", "handle_player_guess", %{
       player: socket.assigns.name,
-      guess: made_guess
+      latest_guess: payload["guessState"],
+      latest_guess_num: payload["lastGuess"]
     })
 
     {:noreply, socket}
@@ -57,7 +71,9 @@ defmodule BaddlWeb.WordleLive do
 
   # handles info broadcast from other LiveViews
   def handle_info(%{topic: _topic, event: "handle_player_guess", payload: payload}, socket) do
-    {:noreply, assign(socket, messages: "hello from #{payload.player}")}
+    socket
+    |> update_player_state(payload)
+    |> then(fn socket -> {:noreply, socket} end)
   end
 
   def handle_info(%{topic: _topic, event: "handle_set_answer", payload: payload}, socket) do
@@ -76,12 +92,29 @@ defmodule BaddlWeb.WordleLive do
     case GameRegistry.get(room_id) do
       nil ->
         # replace with DB query
-        word = Enum.random(~w(place tests space))
+        word = Enum.random(~w(place tests space demos there three slate pacer))
         GameRegistry.set(room_id, word)
         get_answer(room_id)
 
       answer ->
         {:ok, %{answer: answer}}
     end
+  end
+
+  defp update_player_state(socket, %{
+         player: player,
+         latest_guess: latest_guess,
+         latest_guess_num: latest_guess_num
+       }) do
+    socket
+    |> update(:messages, fn messages ->
+      new_player_state =
+        Map.put(%{}, player, %{
+          latest_guess: latest_guess,
+          latest_guess_num: latest_guess_num
+        })
+
+      Map.merge(messages, new_player_state)
+    end)
   end
 end
